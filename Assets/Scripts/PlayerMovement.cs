ccfbfa4b315b38f90ab1ec20c1f00fe4904006db
markmris <mark.mrishih@gmail.com>
2026-01-17 18:13:13 -0400

    Added animations and attacking to movement system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,64 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
+    public Animator animator;
     public float walkSpeed;
     public float jumpHeight;
     public InputActionReference moveAction;
     public InputActionReference jumpAction;
+    public InputActionReference attackAction;
     private Vector2 moveDirection;
-    private bool canJump;
 
-    private void Update()
+    private bool canJump;
+    private bool attacking = false;
+    private bool grounded = false;
+
+    public void Update()
     {
         moveDirection = moveAction.action.ReadValue<Vector2>();
+
+        if (moveDirection.x != 0 && !attacking && grounded)
+        {
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
-        rigidBody.linearVelocity = new Vector2(moveDirection.x * walkSpeed, rigidBody.linearVelocityY);
+        if (!attacking)
+        {
+            rigidBody.linearVelocity = new Vector2(moveDirection.x * walkSpeed, rigidBody.linearVelocityY);
+        }
     }
 
-    private void OnEnable()
+    public void OnEnable()
     {
-        
         jumpAction.action.started += Jump;
+        attackAction.action.started += Attack;
     }
 
-    private void OnDisable()
+    public void OnDisable()
     {
         jumpAction.action.started -= Jump;
+        attackAction.action.started -= Attack;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
+        grounded = true;
+        animator.SetBool("Grounded", true);
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    public void OnCollisionExit2D(Collision2D collision)
     {
         canJump = false;
+        grounded = false;
+        animator.SetBool("Grounded", false);
     }
 
     private void Jump(InputAction.CallbackContext obj)
@@ -48,5 +70,18 @@ public class PlayerMovement : MonoBehaviour
         {
             rigidBody.linearVelocity = Vector2.up * jumpHeight;
         }
+    }
+
+    private void Attack(InputAction.CallbackContext obj)
+    {
+        Debug.Log("ATTACK INPUT");
+        attacking = true;
+        animator.SetTrigger("Attack");
+    }
+
+    public void OnAttackFinished()
+    {
+        attacking = false;
+        Debug.Log("FINISHED ATTACK");
     }
 }
